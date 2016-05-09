1537527a4c0d556cf216ee1a9e229dbbbb67194b
chenper <1548280780@qq.com>
2016-05-09 23:36:44 +0800

    sequence/binaryArray/BinaryTree search

---
+++ src/BinarySearchArray.java
@@ -0,0 +1,82 @@
+public class BinarySearchArray<Key extends Comparable<key>, Value> {
+	private Key[] keys;
+	private Value[] vals;
+	private int N;
+
+	public BinarySearchArray(int capacity) {
+		keys = (Key[]) new Comparable[capacity];
+		vals = (Value[]) new Object[capacity];
+	}
+
+	public int size() {
+		return N;
+	}
+
+	public boolean isEmpty() {
+		return N == 0;
+	}
+
+	public Key min() {
+		return keys[0];
+	}
+
+	public Key max() {
+		return keys[N - 1];
+	}
+
+	public Key select(int k) {
+		return keys[k];
+	}
+
+	public Value get(Key key) {
+		if (isEmpty())
+			return null;
+		int i = rank(key);
+		if (i < N && keys[i].compareTo(key) == 0)
+			return vals[i];
+		else
+			return null;
+	}
+
+	public void delete(Key key) {
+		int i = rank(key);
+		for (int j = i; j < N; ++j) {
+			keys[j] = keys[j + 1];
+			vals[j] = vals[j + 1];
+		}
+		keys[N] = null;
+		vals[N] = null;
+		N--;
+	}
+
+	public void put(Key key, Value val) {
+		int i = rank(key);
+		if (i < N && keys[i].compareTo(key) == 0) {
+			vals[i] = val;
+			return;
+		}
+		for (int j = N; j > i; j--) {
+			keys[j] = keys[j - 1];
+			vals[j] = vals[j - 1];
+		}
+		keys[i] = key;
+		vals[i] = val;
+		N++;
+	}
+
+	public int rank(Key key) {
+		// 有序数组以key排序
+		int lo = 0, hi = N - 1;
+		while (lo <= hi) {
+			int mid = lo + (hi - lo) / 2;
+			int cmp = key.compareTo(keys[mid]);
+			if (cmp < 0)
+				hi = mid - 1;
+			if (cmp > 0)
+				lo = mid + 1;
+			else
+				return mid;
+		}
+		return lo;
+	}
+}
---
+++ src/BinarySearchTree.java
@@ -0,0 +1,164 @@
+
+public class BinarySearchTree<Key extends Comparable<Key>, Value> {
+	private Node root;
+
+	private class Node {
+		private Key key;
+		private Value val;
+		private Node left, right;
+		private int N;
+
+		public Node(Key key, Value val, int N) {
+			this.key = key;
+			this.val = val;
+			this.N = N;
+		}
+	}
+
+	public int size() {
+		return size(root);
+	}
+
+	private int size(Node x) {
+		if (x == null)
+			return 0;
+		else
+			return x.N;
+	}
+
+	public Value get(Key key) {
+		return get(root, key);
+	}
+
+	private Value get(Node x, Key key) {
+		if (x == null)
+			return null;
+		int cmp = key.compareTo(x.key);
+		if (cmp < 0)
+			return get(x.left, key);
+		else if (cmp > 0)
+			return get(x.right, key);
+		else {
+			return x.val;
+		}
+	}
+
+	public void put(Key key, Value val) {
+		root = put(root, key, val);
+	}
+
+	private Node put(Node x, Key key, Value val) {
+		if (x == null)
+			return new Node(key, val, 1);
+		int cmp = key.compareTo(x.key);
+		if (cmp < 0)
+			x.left = put(x.left, key, val);
+		else if (cmp > 0)
+			x.right = put(x.right, key, val);
+		return x;
+	}
+
+	public Key max() {
+		return max(root).key;
+	}
+
+	private Node max(Node x) {
+		if (x.right == null)
+			return x;
+		return max(x.right);
+	}
+
+	public Key min() {
+		return min(root).key;
+	}
+
+	private Node min(Node x) {
+		if (x.left == null)
+			return x;
+		return min(x.left);
+	}
+
+	public Key select(int k) {
+		return select(root, k).key;
+	}
+
+	private Node select(Node x, int k) {
+		if (x == null)
+			return null;
+		int t = size(x.left);
+		if (t > k)
+			return select(x.left, t);
+		else if (t < k)
+			return select(x.right, t - k - 1);
+		else {
+			return x;
+		}
+	}
+
+	public int rank(Key key) {
+		return rank(key, root);
+	}
+
+	private int rank(Key key, Node x) {
+		if (x == null)
+			return 0;
+		int cmp = key.compareTo(x.key);
+		if (cmp < 0)
+			return rank(key, x);
+		else if (cmp > 0)
+			return rank(key, x.right) + size(x.left) + 1;
+		else {
+			return size(x.left);
+		}
+	}
+
+	public void delete(Key key) {
+		root = delete(root, key);
+	}
+
+	private Node delete(Node x, Key key) {
+		if (x == null)
+			return null;
+		int cmp = key.compareTo(x.key);
+		if (cmp < 0)
+			x.left = delete(x.left, key);
+		if (cmp > 0)
+			x.right = delete(x.right, key);
+		else {
+			if (x.right == null)
+				return x.left;
+			if (x.left == null)
+				return x.right;
+			Node t = x;
+			x = min(t.right);
+			x.right = deleteMin(t.right);
+			x.left = t.left;
+		}
+		x.N = size(x.left) + size(x.right) + 1;
+		return x;
+	}
+
+	public void deleteMin() {
+		root = deleteMin(root);
+	}
+
+	private Node deleteMin(Node x) {
+		if (x.left == null)
+			return x.right;
+		x.left = deleteMin(x.left);
+		x.N = size(x.left) + size(x.right) + 1;
+		return x;
+	}
+
+	public void deleteMax() {
+		root = deleteMax(root);
+	}
+
+	private Node deleteMax(Node x) {
+		if (x.right == null)
+			return x.left;
+		x.right = deleteMax(x.right);
+		x.N = size(x.left) + size(x.right) + 1;
+		return x;
+	}
+}
